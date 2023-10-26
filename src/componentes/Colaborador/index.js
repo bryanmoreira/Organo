d9761153dcaedb65511c326729b44869708ba2af
Bryan Moreira <49196485+bryanmoreira@users.noreply.github.com>
2023-10-26 17:50:59 -0300

    second commit

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,42 @@
-import './colaborador.css'
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import './colaborador.css';
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
+    return (
+        <div className="colaborador">
+            <AiFillCloseCircle 
+                size={25} 
+                color='#B01E3A' 
+                className='deletar' 
+                onClick={() => aoDeletar(colaborador.id)} 
+            />
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img 
+                    src={colaborador.imagem} 
+                    alt={colaborador.nome} 
+                />
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+                <div className='favoritar'>
+                    {colaborador.favorito 
+                        ? <AiFillHeart {...propsFavorito} color='#B01E3A' /> 
+                        : <AiOutlineHeart {...propsFavorito} />
+                    }
+                </div>
+            </div>
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+    )
 }
 
-export default Colaborador
+export default Colaborador;
